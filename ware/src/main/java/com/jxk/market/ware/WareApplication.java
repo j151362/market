dff0669c6ad013e2082ab330add58ba1ec9a65c3
code_god <w1240072887@gmail.com>
2022-02-08 23:30:14 +0800

    Initial commit

---
+++ ware/src/main/java/com/jxk/market/ware/WareApplication.java
@@ -0,0 +1,13 @@
+package com.jxk.market.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WareApplication.class, args);
+    }
+
+}
---
+++ ware/src/test/java/com/jxk/market/ware/WareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jxk.market.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
